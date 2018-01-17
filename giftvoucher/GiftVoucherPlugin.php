f0d1dbb4249ad3e21c82e86e1b9ae81fa510b786
Sven Jungnickel <sven@verbb.io>
2018-01-18 09:44:22 +1100

    changelog and version number

--- giftvoucher/GiftVoucherPlugin.php
+++ giftvoucher/GiftVoucherPlugin.php
@@ -19,12 +19,12 @@ class GiftVoucherPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     public function getSchemaVersion()
     {
-        return '1.0.0';
+        return '1.0.1';
     }
 
     public function getDeveloper()
