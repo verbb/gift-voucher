aa86be1d38fb5ab2031ff25d6099049710fac1a4
Josh Crawford <joshua@sgroup.com.au>
2022-03-12 23:38:44 +1100

    Update `minVersionRequired`

--- src/GiftVoucher.php
+++ src/GiftVoucher.php
@@ -60,7 +60,7 @@ class GiftVoucher extends Plugin
     public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
     public string $schemaVersion = '2.0.8';
-    public string $minVersionRequired = '2.0.8';
+    public string $minVersionRequired = '2.4.1';
 
 
     // Traits
