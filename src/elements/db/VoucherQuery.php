cb2877a558569f6c2546745a02039024b931d82e
Josh Crawford <joshua@sgroup.com.au>
2022-04-23 16:10:32 +1000

    Remove plugin version checks, no longer needed

--- src/elements/db/VoucherQuery.php
+++ src/elements/db/VoucherQuery.php
@@ -177,21 +177,12 @@ class VoucherQuery extends ElementQuery
             'giftvoucher_vouchers.sku',
             'giftvoucher_vouchers.price',
             'giftvoucher_vouchers.customAmount',
+            'giftvoucher_vouchers.promotable',
+            'giftvoucher_vouchers.availableForPurchase',
         ]);
 
-        $giftVoucher = Craft::$app->getPlugins()->getStoredPluginInfo('gift-voucher');
-
-        if ($giftVoucher && version_compare($giftVoucher['version'], '2.0.8', '>=')) {
-            $this->query->addSelect(['giftvoucher_vouchers.promotable']);
-            $this->query->addSelect(['giftvoucher_vouchers.availableForPurchase']);
-
-            if ($this->promotable !== null) {
-                $this->subQuery->andWhere(['giftvoucher_vouchers.promotable' => $this->promotable]);
-            }
-
-            if ($this->availableForPurchase !== null) {
-                $this->subQuery->andWhere(['giftvoucher_vouchers.availableForPurchase' => $this->availableForPurchase]);
-            }
+        if ($this->availableForPurchase) {
+            $this->subQuery->andWhere(['giftvoucher_vouchers.availableForPurchase' => $this->availableForPurchase]);
         }
 
         if ($this->postDate) {
