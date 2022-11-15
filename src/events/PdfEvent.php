3b9549a85dbd31c4772d3f1d9bbfc4c66f92df14
Josh Crawford <joshua@sgroup.com.au>
2022-11-15 14:36:55 +1100

    Fix an error when trying to generate a PDF for a voucher in the control panel.

--- src/events/PdfEvent.php
+++ src/events/PdfEvent.php
@@ -7,7 +7,7 @@ use yii\base\Event;
 
 class PdfEvent extends Event
 {
-    public Order $order;
+    public ?Order $order = null;
     public ?string $option = null;
     public ?string $template = null;
     public array $variables = [];
