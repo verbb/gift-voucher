471a8fdd3b107a3035be5fae55b2e90d703f6e2d
Josh Crawford <joshua@sgroup.com.au>
2025-02-19 11:03:50 +1100

    Improve redemption querying performance

--- src/services/Redemptions.php
+++ src/services/Redemptions.php
@@ -7,7 +7,6 @@ use verbb\giftvoucher\records\Redemption as RedemptionRecord;
 
 use Craft;
 use craft\base\Component;
-use craft\base\MemoizableArray;
 use craft\db\Query;
 use craft\helpers\ArrayHelper;
 use craft\helpers\Db;
@@ -25,23 +24,31 @@ class Redemptions extends Component
     public const EVENT_AFTER_DELETE_REDEMPTION = 'afterDeleteRedemption';
 
 
-    // Properties
-    // =========================================================================
-
-    private ?MemoizableArray $_redemptions = null;
-
-
     // Public Methods
     // =========================================================================
 
     public function getRedemptionById(int $id): ?Redemption
     {
-        return $this->_redemptions()->firstWhere('id', $id);
+        $result = $this->_createRedemptionsQuery()
+            ->where(['id' => $id])
+            ->one();
+
+        return $result ? new Redemption($result) : null;
     }
 
     public function getRedemptionsByCodeId(int $codeId): array
     {
-        return $this->_redemptions()->where('codeId', $codeId)->all();
+        $redemptions = [];
+
+        $results = $this->_createRedemptionsQuery()
+            ->where(['codeId' => $codeId])
+            ->all();
+
+        foreach ($results as $result) {
+            $redemptions[] = new Redemption($result);
+        }
+
+        return $redemptions;
     }
 
     public function saveRedemption(Redemption $redemption, bool $runValidation = true): bool
@@ -72,8 +79,6 @@ class Redemptions extends Component
         if ($isNewRedemption) {
             $redemption->id = $redemptionRecord->id;
         }
-
-        $this->_redemptions = null;
 
         if ($this->hasEventHandlers(self::EVENT_AFTER_SAVE_REDEMPTION)) {
             $this->trigger(self::EVENT_AFTER_SAVE_REDEMPTION, new RedemptionEvent([
@@ -120,21 +125,6 @@ class Redemptions extends Component
 
     // Private Methods
     // =========================================================================
-
-    private function _redemptions(): MemoizableArray
-    {
-        if (!isset($this->_redemptions)) {
-            $redemptions = [];
-
-            foreach ($this->_createRedemptionsQuery()->all() as $result) {
-                $redemptions[] = new Redemption($result);
-            }
-
-            $this->_redemptions = new MemoizableArray($redemptions);
-        }
-
-        return $this->_redemptions;
-    }
 
     private function _createRedemptionsQuery(): Query
     {
