92c81162dfd257cd6f44665d98ca0f49a2712a9b
Josh Crawford <joshua@sgroup.com.au>
2025-12-04 09:32:13 +1100

    Fix an error when saving voucher types

--- src/controllers/VoucherTypesController.php
+++ src/controllers/VoucherTypesController.php
@@ -65,7 +65,7 @@ class VoucherTypesController extends Controller
         $voucherType->name = $this->request->getBodyParam('name');
         $voucherType->handle = $this->request->getBodyParam('handle');
         $voucherType->skuFormat = $this->request->getBodyParam('skuFormat');
-        $voucherType->descriptionFormat = $request->getBodyParam('descriptionFormat');
+        $voucherType->descriptionFormat = $this->request->getBodyParam('descriptionFormat');
 
         // Site-specific settings
         $allSiteSettings = [];
